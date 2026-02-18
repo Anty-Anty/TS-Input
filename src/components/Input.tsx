f49b66630eef2aea95d5930f13344d96ac5ee2e8
Anton <ibeliev@gmail.com>
2026-02-18 14:54:19 -0800

    Implement ComponentPropsWithoutRef in Input component to forward native input props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,18 @@
+import type { ComponentPropsWithoutRef } from "react";
+
 type InputProps = {
+    // custom props:
     label: string;
     id: string;
-}
+    
+}   // build-in props of input
+& ComponentPropsWithoutRef<'input'>;
 
-const Input = ({label, id}: InputProps) => {
+const Input = ({label, id, ...props}: InputProps) => {
     return (
         <p>
             <label htmlFor={id}>{label}</label>
-            <input id={id} type="text"/>
+            <input id={id} {...props}/>
         </p>
     )
 }
